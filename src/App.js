186a6031eefbf304c79886dcb28a2c6489e3eb4d
Jonathan Franche <jonfranche@gmail.com>
2022-04-15 10:19:38 -0700

    added dummy info

--- src/App.js
+++ src/App.js
@@ -5,18 +5,51 @@ import Preview from "./components/PreviewComponents/Preview";
 import "./App.css";
 
 const generalInfo = {
-  name: "",
-  email: "",
-  phone: "",
-  city: "",
-  objective: "",
+  name: "John Doe",
+  email: "johndoe@gmail.com",
+  phone: "(123)456-7890",
+  city: "San Francisco, CA",
+  objective:
+    "A passionate Computer Science graduate looking to apply their skills as a Software Engineer. Seeking to use strong programming and development skills gained through side and school projects to help reach the Software Engineering team’s goals.",
 };
 
-const skillsInfo = [];
+const skillsInfo = ["Java", "JavaScript", "ReactJS", "Jira", "Agile"];
 
-const schoolInfo = [];
+const schoolInfo = [
+  {
+    schoolName: "University of California, San Diego",
+    degree: "M.S. Computer Science",
+    city: "San Diego, CA",
+    startDate: "Sept 2021",
+    endDate: "Present",
+  },
+  {
+    schoolName: "San Diego State University",
+    degree: "B.S. Computer Science",
+    city: "San Diego, CA",
+    startDate: "Aug 2016",
+    endDate: "May 2020",
+  },
+];
 
-const expInfo = [];
+const expInfo = [
+  {
+    companyName: "Google",
+    position: "Software Engineer",
+    city: "San Diego, CA",
+    tasks: "- task 1 \n- task 2 \n- task 3",
+    startDate: "June 2020",
+    endDate: "Present"
+  },
+  {
+    companyName: "Qualcomm",
+    position: "Intern",
+    city: "San Diego, CA",
+    tasks: "- task 1 \n- task 2 \n- task 3",
+    startDate: "June 2019",
+    endDate: "Sept 2019"
+  }
+];
 
 const App = () => {
   const [genInfo, setGenInfo] = useState(generalInfo);
@@ -32,7 +65,7 @@ const App = () => {
   const saveSchoolDataHandler = (school) => {
     console.log(school);
     setSchools((prevSchools) => {
-      return [...prevSchools, school];
+      return [school, ...prevSchools];
     });
     console.log(schools);
   };
@@ -40,7 +73,7 @@ const App = () => {
   const saveWorkDataHandler = (work) => {
     console.log(work);
     setExperience((prevExperience) => {
-      return [...prevExperience, work];
+      return [work, ...prevExperience];
     });
   };
 
@@ -75,28 +108,28 @@ const App = () => {
 
   return (
     <div>
-      <Header/>
-    <div className="body-div">
-      <Form
-        generalInfo={genInfo}
-        onSaveGeneralInfo={saveGeneralInfoHandler}
-        onSaveSchoolData={saveSchoolDataHandler}
-        onSaveWorkData={saveWorkDataHandler}
-        onSaveSkills={saveSkillsHandler}
-        onDeleteSkill={deleteSkillHandler}
-        schools={schools}
-        experience={experience}
-        skills={skills}
-      />
-      <Preview
-        generalInfo={genInfo}
-        schools={schools}
-        experience={experience}
-        skills={skills}
-        onDeleteSchool={deleteSchoolHandler}
-        onDeleteExperience={deleteExperienceHandler}
-      />
-    </div>
+      <Header />
+      <div className="body-div">
+        <Form
+          generalInfo={genInfo}
+          onSaveGeneralInfo={saveGeneralInfoHandler}
+          onSaveSchoolData={saveSchoolDataHandler}
+          onSaveWorkData={saveWorkDataHandler}
+          onSaveSkills={saveSkillsHandler}
+          onDeleteSkill={deleteSkillHandler}
+          schools={schools}
+          experience={experience}
+          skills={skills}
+        />
+        <Preview
+          generalInfo={genInfo}
+          schools={schools}
+          experience={experience}
+          skills={skills}
+          onDeleteSchool={deleteSchoolHandler}
+          onDeleteExperience={deleteExperienceHandler}
+        />
+      </div>
     </div>
   );
 };
